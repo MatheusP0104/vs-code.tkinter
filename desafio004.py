8cb80aa8c7a568d1e1983416f1c4b3806f38a4b8
MatheusP0104 <ms2969887@gmail.com>
2022-06-06 16:47:46 -0300

    aula 06/06/22 editado

--- desafio004.py
+++ desafio004.py
@@ -9,15 +9,22 @@ def converter():
         lb2['text'] = 'Caracter Inválido!'
         lb2['foreground'] = 'Red'
 
-
 janela = Tk()
 janela.bind('<Return>', lambda event:converter())
 janela.config(bg='black')
-lb1 = Label(janela, text='C°', font='Arial 22', foreground='Yellow', bg='black')
-lb2 = Label(janela, text='Resultado em F°', font='Arial 22', foreground='yellow', bg='black')
-ety1 = Entry(janela, font='arial 22')
-bt1 = Button(janela, text='Converter', font='arial 22', command=converter, foreground='yellow', bg='black')
 
+fr1 = Frame(janela, bg='black')
+fr2 = Frame(janela, bg='black')
+
+
+lb1 = Label(fr1, text='C°', font='Arial 22', foreground='Yellow', bg='black')
+lb2 = Label(fr2, text='Resultado em F°', font='Arial 22', foreground='yellow', bg='black')
+ety1 = Entry(fr1, font='arial 22')
+bt1 = Button(fr2, text='Converter', font='arial 22', command=converter, foreground='yellow', bg='black')
+
+
+fr1.pack()
+fr2.pack()
 lb1.grid(row=0, column=0)
 lb2.grid(row=1, column=1)
 ety1.grid(row=0, column=1)
